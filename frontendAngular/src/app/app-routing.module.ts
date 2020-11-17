c0f65046209a552040971aecfc385c44557d9f51
Krunal <krunalmiracle@gmail.com>
2020-11-17 20:36:09 +0100

    Frontend prepared Frontend prepared and ready to work on for the exam

--- frontendAngular/src/app/app-routing.module.ts
+++ frontendAngular/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{ path: '', pathMatch: 'full', redirectTo: '/subjects'},Example Path Default of Router
+  //{path: 'auth', component: AuthComponent}, Example
+  //{path: 'admin', component: AdminComponent, canActivate: [AuthGuard]}, Example
+  //{ path: 'newStudent/:subjectName', component: StudentFormComponent}, //Send param in route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
